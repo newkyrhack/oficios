e3e1848d3e5812b3bbab4dbf148cb4e463b1809c
ADFGE\cserrano <kayser.fb@hotmail.com>
2018-05-22 11:54:09 -0500

    una sola migracion

---
+++ database/migrations/2018_05_22_161303_create_tablasoficios.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablasoficios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('secciones_oficios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('etiqueta');
+            $table->longText('html');
+            $table->string('sistema');
+            $table->timestamps();
+        });
+        Schema::create('oficios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('sistema');
+            $table->integer('unidad');
+            $table->timestamps();
+        });
+        Schema::create('templates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('idSeccion')->unsigned();
+            $table->integer('idOficio')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('idSeccion')->references('id')->on('secciones_oficios')->onDelete('restrict');
+            $table->foreign('idOficio')->references('id')->on('oficios')->onDelete('cascade');
+        });
+        Schema::create('oficios_hechos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->longText('html');
+            $table->string('token');
+            $table->string('fiscal');
+            $table->integer('idOficio')->unsigned();
+            $table->timestamps();
+            $table->foreign('idOficio')->references('id')->on('oficios')->onDelete('restrict');            
+        });
+        Schema::create('intentos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('fiscal');
+            $table->integer('idOficio')->unsigned();
+            $table->longText('html');
+            $table->timestamps();
+
+            $table->foreign('idOficio')->references('id')->on('oficios')->onDelete('restrict');            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('secciones_oficios');
+        Schema::dropIfExists('oficios');
+        Schema::dropIfExists('templates');
+        Schema::dropIfExists('oficios_hechos');
+        Schema::dropIfExists('intentos');
+    }
+}
